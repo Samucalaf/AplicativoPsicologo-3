3933a7a34d96bb990d6183491d3413b38257c13e
SamuelLaf <Samuellafuent955@gmail.com>
2025-06-02 21:24:42 -0300

    Implementação dos atributos e metodos de login e logout.

--- src/dados/Usuario.java
+++ src/dados/Usuario.java
@@ -1,5 +1,37 @@
 package dados;
 
 public abstract class Usuario {
+    private String nome;
+    private String email;
+    private String senha;
 
+    public Usuario(String nome, String email, String senha){
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public void login(String email, String senha){
+        if (email.equals(this.email) && senha.equals(this.senha)){
+            System.out.println("Logado com sucesso!");
+        }else{
+            System.out.println("Email ou senha incorretos!");
+        }
+    }
+
+    public void logout(){
+        System.out.println("Saindo da conta...");
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
 }
